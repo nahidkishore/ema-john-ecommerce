3c51f8d35f49b5e47da59a7fa712af049b2e568e
nahid <nahidkishore99@gmail.com>
2020-10-04 20:57:14 +0600

    load data from the server

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Inventory = () => {
   const handleAddProduct=() =>{
-   fetch('http://localhost:5000/addProduct',{
+   fetch('http://localhost:5000/addProducts',{
      method: 'POST',
      headers: {
        'Content-Type': 'application/json'
