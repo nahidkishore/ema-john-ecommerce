bdabf7f1b129efe0e5148a9de6d2214855a929d0
nahid <nahidkishore99@gmail.com>
2020-10-08 21:06:28 +0600

    create payment Process component with simple card form

---
+++ src/components/ProcessPayment/ProcessPayment.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import SimpleCardForm from "./SimpleCardForm";
+const stripePromise = loadStripe("pk_test_51HZtzJBRGc50roq06i3xniicqrgoOLvtYJnfJXeSQVl9fzARwBMf3BzW720VnnJ4wyZvbDUws8wgG4vE35XGf3gj00YHwGFdo4");
+const ProcessPayment = () => {
+  return (
+    <Elements stripe={stripePromise}>
+      <SimpleCardForm />
+    
+    </Elements>
+  );
+};
+
+export default ProcessPayment;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,93 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import './Shop.css';
-import Product from '../Product/Product';
-import Cart from '../Cart/Cart';
-import { addToDatabaseCart, getDatabaseCart } from '../../utilities/databaseManager';
-import { Link } from 'react-router-dom';
-import { CircularProgress } from '@material-ui/core';
+import React, { useState, useEffect } from "react";
+import "./Shop.css";
+import Product from "../Product/Product";
+import Cart from "../Cart/Cart";
+import {
+  addToDatabaseCart,
+  getDatabaseCart,
+} from "../../utilities/databaseManager";
+import { Link } from "react-router-dom";
+import { CircularProgress } from "@material-ui/core";
 
 const Shop = () => {
-  
-//const first10=fakeData.slice(0,10);
-const [products, setProducts]=useState([]);
+  //const first10=fakeData.slice(0,10);
+  const [products, setProducts] = useState([]);
 
-const [cart,setCart]=useState([]);
-// update title based on components
-document.title="Shop more";
+  const [cart, setCart] = useState([]);
+  // update title based on components
+  document.title = "Shop more";
 
-useEffect(() =>{
-fetch('https://serene-sea-68053.herokuapp.com/products')
-.then(res =>res.json())
-.then(data=>setProducts(data))
-},[])
+  useEffect(() => {
+    fetch("https://serene-sea-68053.herokuapp.com/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
-useEffect(()=>{
-  const savedCart=getDatabaseCart();
-  const productKeys=Object.keys(savedCart);
- 
-        fetch('https://serene-sea-68053.herokuapp.com/productsByKeys',{
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(productKeys)
-        })
-        .then(res => res.json())
-        .then(data =>setCart(data))
-      
-      },[])
+  useEffect(() => {
+    const savedCart = getDatabaseCart();
+    const productKeys = Object.keys(savedCart);
 
-const handleAddProduct=(product)=>{
-  const toBeAddedKey=product.key;
-  const sameProduct=cart.find(pd=>pd.key === toBeAddedKey);
-  let count=1;
-  let newCart;
-  if(sameProduct){
-    
-    count=sameProduct.quantity + 1;
-    sameProduct.quantity= count;
-    const others=cart.filter(pd=>pd.key !==toBeAddedKey);
-    newCart=[...others, sameProduct];
-  }
-  else{
-    product.quantity=1;
-    newCart=[...cart, product];
-  }
-  
-  setCart(newCart);
- 
-  addToDatabaseCart(product.key,count);
-}
+    fetch("https://serene-sea-68053.herokuapp.com/productsByKeys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productKeys),
+    })
+      .then((res) => res.json())
+      .then((data) => setCart(data));
+  }, []);
 
+  const handleAddProduct = (product) => {
+    const toBeAddedKey = product.key;
+    const sameProduct = cart.find((pd) => pd.key === toBeAddedKey);
+    let count = 1;
+    let newCart;
+    if (sameProduct) {
+      count = sameProduct.quantity + 1;
+      sameProduct.quantity = count;
+      const others = cart.filter((pd) => pd.key !== toBeAddedKey);
+      newCart = [...others, sameProduct];
+    } else {
+      product.quantity = 1;
+      newCart = [...cart, product];
+    }
+
+    setCart(newCart);
+
+    addToDatabaseCart(product.key, count);
+  };
 
   return (
     <div className="twin-container">
       <div className="product-container">
-       {
-        products.length ===0 &&   <CircularProgress color="secondary" />
-      } 
-        {
-          products.map(pd=> <Product 
+        {products.length === 0 && <CircularProgress color="secondary" />}
+        {products.map((pd) => (
+          <Product
             key={pd.key}
             showAddToCart={true}
-                handleAddProduct={handleAddProduct}
-          
-                product={pd}>
-            
-                   </Product>)
-        }
-      
-   
+            handleAddProduct={handleAddProduct}
+            product={pd}
+          ></Product>
+        ))}
       </div>
       <div className="cart-container">
         <Cart cart={cart}></Cart>
-        <Link to="/review"><button className="main-button">Review Order</button>
-      </Link>
-
+        <Link to="/review">
+          <button className="main-button">Review Order</button>
+        </Link>
       </div>
-      
-     
-      
     </div>
   );
 };
