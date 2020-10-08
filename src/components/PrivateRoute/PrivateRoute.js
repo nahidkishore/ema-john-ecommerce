e8934d0b15eaa8e7841819e546623cbcda491be6
nahid <nahidkishore99@gmail.com>
2020-10-08 22:11:40 +0600

    Use detail credit card from and avoid login after page refresh

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,25 +1,25 @@
-import React, { useContext } from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import { UserContext } from '../../App';
+import React, { useContext } from "react";
+import { Route, Redirect } from "react-router-dom";
+import { UserContext } from "../../App";
 
-const PrivateRoute = ({children,...rest}) => {
-  const [loggedInUser, setLoggedInUser]=useContext(UserContext);
+const PrivateRoute = ({ children, ...rest }) => {
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
   return (
     <Route
-    {...rest}
-    render={({ location }) =>
-    loggedInUser.email ? (
-        children
-      ) : (
-        <Redirect
-          to={{
-            pathname: "/login",
-            state: { from: location }
-          }}
-        />
-      )
-    }
-  />
+      {...rest}
+      render={({ location }) =>
+        loggedInUser.email || sessionStorage.getItem("token") ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
   );
 };
 
